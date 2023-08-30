ceda0d43f5c9e64ba7c36ac98e2fafbeccae65de
kabalport <kabalport@gmail.com>
2023-08-31 02:26:40 +0900

    init3

---
+++ src/test/java/com/example/springbootdeveloper/config/jwt/TokenProviderTest.java
@@ -0,0 +1,21 @@
+package com.example.springbootdeveloper.config.jwt;
+
+import com.example.springbootdeveloper.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TokenProviderTest {
+    @Autowired
+    private TokenProvider tokenProvider;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    // generateToken() 검증테스트
+
+
+}
